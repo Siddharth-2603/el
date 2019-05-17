5931472ada20c69654a18a0fe2aa47cf5cfbee34
Lukas Fam <takeruadelbert@gmail.com>
2019-05-17 14:05:24 +0700

    update script to install all required packages

--- install-package.sh
+++ install-package.sh
@@ -3,6 +3,15 @@
 # make script executable
 sudo chmod +x install-package.sh
 
+# install mysql-client if it's not installed yet
+if [ -f /etc/init.d/mysql* ]; then
+	echo "mysql-client is already installed."
+else
+	echo "Installing mysql-client..."
+	sudo apt-get install mysql-client
+	echo "Successfully installed mysql-client."
+fi
+
 # install required packages for Python 3
 declare -a packages=("mysql-connector" "xlsxwriter" "tkcalendar" "babel")
 for i in "${packages[@]}"
