362d53ba048cb5b4359b95e9cf0d9fcf6ee6da01
Lukas Fam <takeruadelbert@gmail.com>
2019-05-17 13:51:34 +0700

    update script to install required packages

--- install-package.sh
+++ install-package.sh
@@ -8,5 +8,8 @@ declare -a packages=("mysql-connector" "xlsxwriter" "tkcalendar" "babel")
 for i in "${packages[@]}"
 do
 #	echo "$i"
+	echo "Installing $i ..."
 	sudo pip3 install "$i"
 done
+
+echo "Done Installing all required packages."
